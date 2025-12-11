a1e71585e23f85dc2ed42cf70838782d98faebd9
RajdipRudra <rajdiprudra100@gmail.com>
2025-12-12 04:25:43 +0600

    umm yah i need to get back to work

---
+++ testing_myskillP2/cha.py
@@ -0,0 +1,4 @@
+# Create a 3D array using list comprehensions, with 3 rows, 4 columns, and each element initialized as '*'
+array = [[['*' for col in range(6)] for col in range(4)] for row in range(3)]
+
+# Print the resulting 3D array
